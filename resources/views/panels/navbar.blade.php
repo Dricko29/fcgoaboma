6d38f63ae2794944d82872b34f538f6130b6e1c7
Dricko29 <dricko0798@gmail.com>
2022-11-14 07:47:33 +0700

    install lang id

--- resources/views/panels/navbar.blade.php
+++ resources/views/panels/navbar.blade.php
@@ -85,10 +85,13 @@
   <ul class="nav navbar-nav align-items-center ms-auto">
     <li class="nav-item dropdown dropdown-language">
       <a class="nav-link dropdown-toggle" id="dropdown-flag" href="#" data-bs-toggle="dropdown" aria-haspopup="true">
-        <i class="flag-icon flag-icon-us"></i>
-        <span class="selected-language">English</span>
+        <i class="flag-icon flag-icon-id"></i>
+        <span class="selected-language">Indonesia</span>
       </a>
       <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-flag">
+        <a class="dropdown-item" href="{{ url('lang/id') }}" data-language="id">
+          <i class="flag-icon flag-icon-id"></i> Indonesia
+        </a>
         <a class="dropdown-item" href="{{ url('lang/en') }}" data-language="en">
           <i class="flag-icon flag-icon-us"></i> English
         </a>
